74f7901de3e0102069c786e18a4b20c076f6218f
wanjiban <109531247+wanjiban@users.noreply.github.com>
2024-06-06 09:07:27 +0800

    Update diy.sh

--- scripts/master/diy.sh
+++ scripts/master/diy.sh
@@ -9,7 +9,7 @@ rm -rf ./feeds/packages/net/chinadns-ng
 rm -rf package/boot/uboot-rockchip/src/arch/arm/dts/rk3328-orangepi-r1-plus-lts-u-boot.dtsi
 
 #更改默认地址为192.168.8.1
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 
 #更改r8152默认驱动
 #sed -i 's/kmod-usb-net-rtl8152/kmod-usb-net-rtl8152-vendor/g' target/linux/rockchip/image/armv8.mk
