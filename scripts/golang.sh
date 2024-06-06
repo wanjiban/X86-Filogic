37460709c1336a0240ab1114435a1339f9e3de6b
wanjiban <109531247+wanjiban@users.noreply.github.com>
2024-06-06 16:01:49 +0800

    Update golang.sh

--- scripts/golang.sh
+++ scripts/golang.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+#=================================================
+# File name: preset-clash-core.sh
+# Usage: <preset-clash-core.sh $platform> | example: <preset-clash-core.sh armv8>
+# System Required: Linux
+# Version: 1.0
+# Lisence: MIT
+# Author: SuLingGG
+# Blog: https://mlapp.cn
+# 参考网址：https://github.com/zzcabc/OpenWrt_Action/blob/11208c3d5160128d22d14318772ec48f1918deb9/script/immortalwrt/diy2.sh
+#=================================================
+
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
