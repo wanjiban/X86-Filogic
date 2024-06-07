6df17115448990304e8245866c5993e65c65a2c8
wanjiban <109531247+wanjiban@users.noreply.github.com>
2024-06-07 14:03:03 +0800

    Update preset-clash-core-arm64.sh

--- scripts/preset-clash-core-arm64.sh
+++ scripts/preset-clash-core-arm64.sh
@@ -31,21 +31,22 @@ CLASH_TUN_URL="https://github.com/vernesong/OpenClash/raw/core/master/premium/cl
 # CLASH_TUN_URL=$(curl -sL https://api.github.com/repos/Dreamacro/clash/releases/tags/premium | grep /clash-linux-amd64-2 | awk -F '"' '{print $4}' | head -n 1)
 
 # Meta内核版本
-CLASH_META_URL=$( curl -sL https://api.github.com/repos/MetaCubeX/Clash.Meta/releases/tags/Prerelease-Alpha | grep /mihomo-linux-arm64-alpha | awk -F '"' '{print $4}' | head -n 1)
+# CLASH_META_URL=$( curl -sL https://api.github.com/repos/MetaCubeX/Clash.Meta/releases/tags/Prerelease-Alpha | grep /mihomo-linux-arm64-alpha | awk -F '"' '{print $4}' | head -n 1)
 
 # CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/dev/clash-linux-arm64.tar.gz"
 # CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/core-lateset/premium | grep download_url | grep $1 | awk -F '"' '{print $4}')
 # CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/meta/clash-linux-${1}.tar.gz"
+CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/meta/clash-linux-amd64.tar.gz"
 
 
 # 给内核解压
 wget -qO- $CLASH_DEV_URL | tar xOvz > files/etc/openclash/core/clash
 wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
-# wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
+wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
 
 # wget -qO- $CLASH_DEV_URL | gunzip -c > files/etc/openclash/core/clash
 # wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
-wget -qO- $CLASH_META_URL | gunzip -c > files/etc/openclash/core/clash_meta
+# wget -qO- $CLASH_META_URL | gunzip -c > files/etc/openclash/core/clash_meta
 
 # 给内核权限
 chmod +x files/etc/openclash/core/clash*
