00b98c6dcdd1d443f1ee4278a0bf9471ba472c5b
wanjiban <109531247+wanjiban@users.noreply.github.com>
2024-06-08 09:04:52 +0800

    Update golang-part2.sh

--- scripts/golang-part2.sh
+++ scripts/golang-part2.sh
@@ -10,45 +10,52 @@
 # 参考网址：https://github.com/zzcabc/OpenWrt_Action/blob/11208c3d5160128d22d14318772ec48f1918deb9/script/immortalwrt/diy2.sh
 #=================================================
 
-CONFIGDIR="package/base-files/files/bin/config_generate"
-
-
-
 # todo
 # package/base-files/files/etc/board.d/99-default_network
 # package/emortal/default-settings/files/99-default-settings-chinese
 # hostname
 
-# 修复 GOLANG 版本
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
-
-
-# 修改生成的固件名称include/image.mk
-sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX:=$(shell date +%Y%m%d)' include/image.mk
-sed -i '/IMG_PREFIX:=/s/^#\?/#/' include/image.mk
-sed -i '/IMG_PREFIX_VERCODE:=/a\IMG_PREFIX:=wayos-$(BUILD_DATE_PREFIX)' include/image.mk 
+#
+# MAIN
+#
 
 # LAN LAN LAN LAN 
 sed -i "/uci commit/a uci commit network"  $CONFIGDIR
-sed -i "/uci commit network/i uci set network.lan.ifname='eth0 eth1 eth2 eth3'"  $CONFIGDIR
-sed -i "/uci commit network/i uci set network.lan.dns='61.139.2.69 223.5.5.5'"  $CONFIGDIR
-sed -i "/uci commit network/i uci set network.lan.gateway='10.1.12.1'"  $CONFIGDIR
-#sed -i "/uci commit network/i uci set network.wan.ifname='xeth0'"  $CONFIGDIR
+sed -i "/uci commit network/i uci set network.wan.ifname='eth0'"  $CONFIGDIR
+sed -i "/uci commit network/i uci set network.lan.ifname='eth1 eth2 eth3'"  $CONFIGDIR
+#sed -i "/uci commit network/i uci set network.lan.dns='61.139.2.69 223.5.5.5'"  $CONFIGDIR
+#sed -i "/uci commit network/i uci set network.lan.gateway='10.1.12.1'"  $CONFIGDIR
 #sed -i "/uci commit network/i uci set network.wan6.ifname='xeth0'"  $CONFIGDIR
 #sed -i "/uci commit network/i uci set network.wan.proto='none'"  $CONFIGDIR
-sed -i "/uci commit network/i uci set dhcp.lan.ignore='1'"  $CONFIGDIR
+#sed -i "/uci commit network/i uci set dhcp.lan.ignore='1'"  $CONFIGDIR
 #sed -i "/uci commit network/i uci delete network.wan6"  $CONFIGDIR
 #sed -i "/uci commit network/i uci delete network.lan.ip6assign"  $CONFIGDIR
 #sed -i "/uci commit network/i uci delete network.globals.ula_prefix"  $CONFIGDIR
-sed -i "/uci commit system/a uci commit dhcp"  $CONFIGDIR
+#sed -i "/uci commit system/a uci commit dhcp"  $CONFIGDIR
 #sed -i "/uci commit dhcp/i uci delete dhcp.lan.ra"  $CONFIGDIR
 #sed -i "/uci commit dhcp/i uci delete dhcp.lan.dhcpv6"  $CONFIGDIR
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' $CONFIGDIR
 #sed -i 's/255.255.0.0/255.255.255.0/g' $CONFIGDIR
-sed -i 's/192.168.1.1/10.1.12.222/g' $CONFIGDIR
+sed -i 's/192.168.1.1/10.0.0.1/g' $CONFIGDIR
+
+
+# COMMON
+
+CONFIGDIR="package/base-files/files/bin/config_generate"
+
+# 修复 GOLANG 版本
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+
+# 修改生成的固件名称include/image.mk
+sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX:=$(shell date +%Y%m%d)' include/image.mk
+sed -i '/IMG_PREFIX:=/s/^#\?/#/' include/image.mk
+sed -i '/IMG_PREFIX_VERCODE:=/a\IMG_PREFIX:=wayos-$(BUILD_DATE_PREFIX)' include/image.mk
+sed -i "/ImmortalWrt/i WayOS" include/version.mk
+# Set DISTRIB_REVISION
+# sed -i "s/OpenWrt /Way Build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # DIAG
 sed -i "/uci commit/a uci commit luci.diag"  $CONFIGDIR
@@ -62,8 +69,6 @@ sed -i "/uci commit diag/i uci set luci.diag.route='jd.com'"  $CONFIGDIR
 # 移动光标至第一格(by:kokang)
 #sed -i "s/'luci_password'/'luci_username'/g" feeds/luci/modules/luci-base/luasrc/view/sysauth.htm
 #sed -i "s/'luci_password'/'luci_username'/g" feeds/kenzo/luci-theme-argonne/luasrc/view/themes/argone/sysauth.htm
-
-
 
 # Modify system
 sed -i 's/ImmortalWrt/Way/g' $CONFIGDIR
@@ -85,7 +90,7 @@ sed -i "/uci commit system/a uci commit ttyd"  $CONFIGDIR
 sed -i "/uci commit ttyd/i uci set ttyd.@ttyd[0].interface='@lan @wan'"  $CONFIGDIR
 
 
-# FW
+# Firewall
 sed -i "/uci commit/a uci commit firewall"  $CONFIGDIR
 sed -i "/uci commit firewall/i uci set firewall.web=rule"  $CONFIGDIR
 sed -i "/uci commit firewall/i uci set firewall.web.target='ACCEPT'"  $CONFIGDIR
@@ -113,6 +118,7 @@ sed -i "/uci commit firewall/i uci set firewall.ttyd.enabled='0'"  $CONFIGDIR
 #rm -rf package/base-files/files/etc/banner
 # 插入信息到 banner
 # 在 cat >> .config <<EOF 到 EOF 之间粘贴你的编译配置, 需注意缩进关系
+
 cat >> package/base-files/files/etc/banner <<EOF
 -----------------------------------------------------
  PACKAGE:     $OMR_DIST
@@ -140,9 +146,6 @@ sed -i "s/\/bin\/ash/\/bin\/bash/" package/base-files/files/usr/libexec/login.sh
 # SSH open to all
 sed -i '/option Interface/s/^#\?/#/'  package/network/services/dropbear/files/dropbear.config
 
-# Set DISTRIB_REVISION
-sed -i "s/OpenWrt /Way Build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
-
 # OPKG
 #sed -i 's#mirrors.cloud.tencent.com/lede#mirrors.tuna.tsinghua.edu.cn/openwrt#g' package/lean/default-settings/files/zzz-default-settings
 #sed -i 's/x86_64/x86\/64/' /etc/opkg/distfeeds.conf
@@ -157,7 +160,6 @@ sed -i "s/OpenWrt /Way Build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/
 #uci set network.lan.ip6assign='60'
 #uci commit
 #EOF
-
 
 
 # 修改DHCP
@@ -176,8 +178,8 @@ sed -i 's/150/250/g' package/network/services/dnsmasq/files/dhcp.conf
 # Password
 # sed -i '/shadow/s/^#\?/# /' $CONFIGDIR
 sed -i '/shadow/d' $CONFIGDIR
-sed -i 's/root::0:0:99999:7:::/root:$1$P4yrmMQf$XRoELeUToXNeituE0pl22.:19131:0:99999:7:::/g' package/base-files/files/etc/shadow
-# sed -i 's/root:::0:99999:7:::/root:$1$P4yrmMQf$XRoELeUToXNeituE0pl22.:19131:0:99999:7:::/g' package/base-files/files/etc/shadow
+# sed -i 's/root::0:0:99999:7:::/root:$1$P4yrmMQf$XRoELeUToXNeituE0pl22.:19131:0:99999:7:::/g' package/base-files/files/etc/shadow
+sed -i 's/root:::0:99999:7:::/root:$1$P4yrmMQf$XRoELeUToXNeituE0pl22.:19131:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 
 # 设置密码为空
